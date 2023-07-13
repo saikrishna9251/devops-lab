97e4820c8710e9705c40a4db3fd1202293c4b5ed
sai krishna <saikrishna.sk684@gmail.com>
2023-07-13 23:14:23 +0530

    added userdata-ssm

---
+++ userdata-ssm.sh
@@ -0,0 +1,7 @@
+#USERDATA:
+========= 
+#!/bin/bash
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl start amazon-ssm-agent
+sudo systemctl enable amazon-ssm-agent
+sudo systemctl status amazon-ssm-agent
